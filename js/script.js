3e1a6fa1c53614dc3b1938446bdacec7af9d20bd
Nino Chimento <saninoc84@gmail.com>
2020-01-03 12:32:51 +0100

    creazione animazione giro carte inizio gioco

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-
+document.getElementById('play').addEventListener("click",
+function(){
+  document.getElementById("card1").classList.add("rotation");
+  document.getElementById("card2").classList.add("rotation");
+  document.getElementById("card3").classList.add("rotation");
+  document.getElementById("card4").classList.add("rotation");
+  document.getElementById("card5").classList.add("rotation");
+  document.getElementById("card6").classList.add("rotation");
+  document.getElementById("card7").classList.add("rotation");
+  document.getElementById("card8").classList.add("rotation");
+  document.getElementById("card9").classList.add("rotation");
+  document.getElementById("card10").classList.add("rotation");
+  document.getElementById("card11").classList.add("rotation");
+  document.getElementById("card12").classList.add("rotation");
+  document.getElementById("card13").classList.add("rotation");
+  document.getElementById("card14").classList.add("rotation");
+  document.getElementById("card15").classList.add("rotation",)
+  document.getElementById("card16").classList.add("rotation");
+  setTimeout(function () {
+    document.getElementById('card1').classList.add("bootstramp");
+    document.getElementById("card2").classList.add("bootstramp");
+    document.getElementById("card3").classList.add("csharp");
+    document.getElementById("card4").classList.add("csharp");
+    document.getElementById("card5").classList.add("css3");
+    document.getElementById("card6").classList.add("css3");
+    document.getElementById("card7").classList.add("html5");
+    document.getElementById("card8").classList.add("html5");
+    document.getElementById("card9").classList.add("java");
+    document.getElementById("card10").classList.add("java");
+    document.getElementById("card11").classList.add("laravel");
+    document.getElementById("card12").classList.add("laravel");
+    document.getElementById("card13").classList.add("php");
+    document.getElementById("card14").classList.add("php");
+    document.getElementById("card15").classList.add("python");
+    document.getElementById("card16").classList.add("python");
+  }, 200);
+}
+)
